46e034ae45016249a246d4ebb9ca4b73dba971c0
Marjan <moonmarjanhasan@gmail.com>
2025-08-22 20:18:17 +0600

    feat(Banner): banner component, image, css, fully responsiveness added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Banner from "./components/Banner/Banner";
 import OfferHeadings from "./components/OfferHeadings/OfferHeadings";
 
 const App = () => {
   return (
-    <div>
+    <div className="max-w-[1440px] mx-auto">
       <title>HypeManbd</title>
       {/* Offer Headings showing at the top */}
       <OfferHeadings />
+      {/* Banner component showing the image slider */}
+      <Banner />
     </div>
   );
 };
